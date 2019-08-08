815a42e1f5b1bad576b0f61f83d03f82ade17518
ea777 <41474742+ea777@users.noreply.github.com>
2019-08-08 09:42:32 +0100

    Update route.js

--- route.js
+++ route.js
@@ -39,6 +39,15 @@ const router = app => {
                 response.send(result);
         });
     });	
+	
+	 // Display all the details for the accesspoint of the id I passed in.
+    app.get('/accesspoint/:id/', (request, response) => {
+	const id = request.params.id;
+        pool.query('SELECT * FROM switch WHERE idaccesspoint = ?', id, (error, result) => {
+                if (error) throw error;
+                response.send(result);
+        });
+    });	
     
     // // Display all sensors
     // app.get('/count_sensors', (request, response) => {
